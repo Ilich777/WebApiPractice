ff016eaf144bc0e476d26ed4bf242848fbb46f75
Ilya <iliya.zhukov138@gmail.com>
2022-01-27 11:10:04 +0600

    Убираю конфликты

--- WebApiPractice/Entities/Dataset.cs
+++ WebApiPractice/Entities/Dataset.cs
@@ -9,25 +9,15 @@ namespace WebApiPractice.Entities
     public class Dataset //модель для записи данных в БД
     {
         public int id { get; init; } //id записи
-
-        public string phone { get; set; } //телефон пользователя
-
-        public string email { get; set; } //почта пользователя
-
-        public DateTime dateTime { get; set; } //дата+время
-
-        public bool finished { get; set; } //завершен
-
-        public bool cancelled { get; set; } //отменён
         [Required(ErrorMessage = "Укажите Подразделение")]
         public string division { get; init; }
-        //public virtual Divisions division { get; set; } //переменная класса подразделения
-
-        public int divisionID { get; set; } //id подразделения
-
         public string service { get; init; }
-        //public virtual Services service { get; set; } //переменная класса услуг
+        public string date { get; init; }
+        public string time { get; init; }
+        public string email { get; set; } //почта пользователя
+        public string phone { get; set; } //телефон пользователя
+        public bool finished { get; set; } //завершен
+        public bool cancelled { get; set; } //отменён
 
-        public int serviceID { get; set; } //id услуги
     }
 }
