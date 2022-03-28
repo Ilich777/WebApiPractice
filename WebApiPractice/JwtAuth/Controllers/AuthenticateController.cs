c425730089f77fea2a4f8df1f74442bb54e64760
IZhukov <IZhukov@TP-VI-IV>
2022-03-28 09:54:21 +0600

    3-4часа(изменение)

--- WebApiPractice/JwtAuth/Controllers/AuthenticateController.cs
+++ WebApiPractice/JwtAuth/Controllers/AuthenticateController.cs
@@ -126,7 +126,7 @@ namespace WebApiPractice.JwtAuth.Auth
             var token = new JwtSecurityToken(
                 issuer: _configuration["JWT:ValidIssuer"],
                 audience: _configuration["JWT:ValidAudience"],
-                expires: DateTime.Now.AddHours(3),
+                expires: DateTime.Now.AddHours(4),
                 claims: authClaims,
                 signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
                 );
